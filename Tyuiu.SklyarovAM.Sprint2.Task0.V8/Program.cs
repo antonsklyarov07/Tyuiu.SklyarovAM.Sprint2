de5bdd7c0dacfc0ea187abb17e1e44a7523e8da3
Антон Скляров <sklarovanton833@gmail.com>
2026-06-08 11:57:38 +0500

    Task0

--- Tyuiu.SklyarovAM.Sprint2.Task0.V8/Program.cs
+++ Tyuiu.SklyarovAM.Sprint2.Task0.V8/Program.cs
@@ -14,19 +14,6 @@ namespace Tyuiu.SklyarovAM.Sprint2.Task0.V8
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            Console.Title = "Спринт #2 | Выполнил: Скляров А. М. | ИИПб-25-1";
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Спринт #2                                                               *");
-            Console.WriteLine("* Тема: Операции сравнения                                                *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #8                                                              *");
-            Console.WriteLine("* Выполнил: Скляров А. М. | ИИПб-25-1                                     *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
-            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
-            Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
-            Console.WriteLine("* (True, False, True, False, True, False), при x = 185, y = 251           *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
